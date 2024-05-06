385accb4c9e3b5980acbaa0e5c5a250e859c591d
DanielRomeroGO <daniel.romerogo.14@gmail.com>
2024-05-06 18:46:43 +0200

    Creacion de ventas y CochesVendidos

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ from routes.marcaCoche import marcaCoche
 from routes.imagenCoche import imagenCoche
 from routes.usuario import usuario
 from routes.modeloCoche import modeloCoche
-<<<<<<< HEAD
-from routes.cocheVendido import cocheVendido
-from routes.venta import venta
-=======
 from routes.ventas import ventas
 from routes.cochesVendidos import cocheVendido
->>>>>>> 3ba74ff5e3caba85aa2dc56bda2731574d5e3646
 from config.openapi import tags_metadata
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -34,10 +29,5 @@ app.include_router(marcaCoche)
 app.include_router(imagenCoche)
 app.include_router(usuario)
 app.include_router(modeloCoche)
-<<<<<<< HEAD
-app.include_router(cocheVendido)
-app.include_router(venta)
-=======
 app.include_router(ventas)
 app.include_router(cocheVendido)
->>>>>>> 3ba74ff5e3caba85aa2dc56bda2731574d5e3646
--- schemas/Venta.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class VentaBase(BaseModel):
-    id: int
-    coche_id: int
-    comprador_id: int
