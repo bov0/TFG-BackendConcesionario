87edbfb803682b9914b039e20c351a94506a234a
Ivan <ibay2002@gmail.com>
2024-06-03 22:53:22 +0200

    Conexion a google cloud

--- config/db.py
+++ config/db.py
@@ -5,6 +5,12 @@ MYSQLDB_USUARIO = "ivan_fdez02"
 MYSQLDB_PASSWORD = "palomeras"
 MYSQLDB_BD = "iesdawivan02"
 
+#Conexión a Google Cloud SQL
+#MYSQLDB_HOST = "34.175.31.241"
+#MYSQLDB_USUARIO = "admin"
+#MYSQLDB_PASSWORD = "admin1234"
+#MYSQLDB_BD = "iesdawivan02"
+
 db_url = f"mysql+pymysql://{MYSQLDB_USUARIO}:{MYSQLDB_PASSWORD}@{MYSQLDB_HOST}/{MYSQLDB_BD}"
 
 engine = create_engine(db_url)
